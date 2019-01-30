e760bcb46ee5a966e4c04fb66b8d3aba888255ef
Sébastien M.L <sebastien.lejeune6@gmail.com>
2019-01-30 14:46:40 +0100

    1st commit

---
+++ Credit/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Credit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mensu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capital = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CapitalRestant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sauverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            pictureBox1.Location = new System.Drawing.Point(412, 135);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(98, 88);
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(412, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Montant emprunté :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Taux annuel du crédit :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Durée de remboursement :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "CALCULEZ VOTRE CREDIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "AFFICHER L\'ECHEANCIER";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num,
+            this.Mensu,
+            this.capital,
+            this.inter,
+            this.CapitalRestant});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 229);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 319);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "Numéro";
+            this.Num.Name = "Num";
+            this.Num.ReadOnly = true;
+            // 
+            // Mensu
+            // 
+            this.Mensu.HeaderText = "Mensualité";
+            this.Mensu.Name = "Mensu";
+            this.Mensu.ReadOnly = true;
+            // 
+            // capital
+            // 
+            this.capital.HeaderText = "Capital Remboursé";
+            this.capital.Name = "capital";
+            this.capital.ReadOnly = true;
+            // 
+            // inter
+            // 
+            this.inter.HeaderText = "Intérêt";
+            this.inter.Name = "inter";
+            this.inter.ReadOnly = true;
+            // 
+            // CapitalRestant
+            // 
+            this.CapitalRestant.HeaderText = "Capital Restant";
+            this.CapitalRestant.Name = "CapitalRestant";
+            this.CapitalRestant.ReadOnly = true;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Salary";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(570, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series3.Legend = "Salary";
+            series3.Name = "interet";
+            series3.YValuesPerPoint = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series4.Legend = "Salary";
+            series4.Name = "montant";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(559, 536);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(277, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "AFFICHER LE GRAPHIQUE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1141, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sauverToolStripMenuItem,
+            this.ouvrirToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // sauverToolStripMenuItem
+            // 
+            this.sauverToolStripMenuItem.Name = "sauverToolStripMenuItem";
+            this.sauverToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.sauverToolStripMenuItem.Text = "Sauver";
+            this.sauverToolStripMenuItem.Click += new System.EventHandler(this.sauverToolStripMenuItem_Click);
+            // 
+            // ouvrirToolStripMenuItem
+            // 
+            this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
+            this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.ouvrirToolStripMenuItem.Text = "Ouvrir";
+            this.ouvrirToolStripMenuItem.Click += new System.EventHandler(this.ouvrirToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Salaire :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 560);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Crédit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mensu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn capital;
+        private System.Windows.Forms.DataGridViewTextBoxColumn inter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CapitalRestant;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sauverToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Credit/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace Credit
+{
+    public partial class Form1 : Form
+    {
+
+        double CapitalRembourse, salaire, InteretMens, Rembours, TauxM, taux, ResuM, PrixTotalCredit, ResuTot1, duree, montant, dureeCalc, test = 0.0;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ouvrir = new OpenFileDialog();
+            if (ouvrir.ShowDialog() == DialogResult.OK)
+            {
+                ouvrir.OpenFile();
+            }
+        }
+
+        private void sauverToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextWriter sauvegarde = new StreamWriter(@"F:\Sauvegarde.txt");
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int l = 0; l < dataGridView1.Columns.Count; l++)
+                {
+                    sauvegarde.Write("\t" + dataGridView1.Rows[i].Cells[l].Value.ToString() + "\t" + "|");
+                }
+                sauvegarde.WriteLine("");
+                sauvegarde.WriteLine("---------------------------------------------------------------------------------------------------------------");
+            }
+            sauvegarde.Close();
+            MessageBox.Show("Données Saved");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            duree = double.Parse(comboBox1.SelectedItem.ToString());
+            montant = double.Parse(textBox1.Text);
+            salaire = double.Parse(textBox2.Text);
+            duree *= 12;
+            TauxM = (taux / 100) / 12;
+            if (testSalaire(salaire, ResuM) == false)
+                MessageBox.Show("Vous ne gagnez pas assez d'argent par mois pour la durée de votre crédit");
+            else
+            {
+                chart1.Visible = true;
+                for (int i = 0; i < duree; i++)
+                {
+                    InteretMens = montant * TauxM; //interet 
+
+                    CapitalRembourse = ResuM - InteretMens;  //Capital Rembourse
+                    montant = (montant - ResuM) + InteretMens; //Nouveau Montant
+
+                    CapitalRembourse = Math.Round(CapitalRembourse, 2);//Arrondi 10-2
+                    InteretMens = Math.Round(InteretMens, 2);//Arrondi 10-2
+                    montant = Math.Round(montant, 2); //Arrondi 10-2
+                    InteretMens = montant * TauxM; //interet 
+                    chart1.Series["montant"].Points.AddXY(i, CapitalRembourse);
+                    chart1.Series["interet"].Points.AddXY(i, InteretMens);
+                }
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i < 31; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            textBox2.PasswordChar = '*';
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            duree = double.Parse(comboBox1.SelectedItem.ToString());
+            montant = double.Parse(textBox1.Text);
+            taux = double.Parse(textBox3.Text);
+            salaire = double.Parse(textBox2.Text);
+            duree *= 12;
+            dureeCalc = duree * -1;
+            TauxM = (taux / 100) / 12;
+            ResuM = (montant * TauxM) / (1 - Math.Pow(1 + TauxM, dureeCalc));
+            ResuM = Math.Round(ResuM, 2);
+            PrixTotalCredit = ResuM * duree;
+            string ResultatMensu = ResuM.ToString();
+            string PrixTotalCreditStr = PrixTotalCredit.ToString();
+            if (testSalaire(salaire, ResuM) == false)
+                MessageBox.Show("Vous ne gagnez pas assez d'argent par mois pour la durée de votre crédit");
+            else
+            {
+                label4.Text = "Montant de la mensualité : " + ResultatMensu + " € ";
+                label5.Text = "Coût total de ce crédit : " + PrixTotalCreditStr + " €";
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+            string id, ResultatTotal, ResultatMensu, interetMensStr, RemboursementStr = "";
+            duree = double.Parse(comboBox1.SelectedItem.ToString());
+            montant = double.Parse(textBox1.Text);
+            taux = double.Parse(textBox3.Text);
+            salaire = double.Parse(textBox2.Text);
+            duree *= 12;
+            TauxM = (taux / 100) / 12;
+            ResuM = (montant * TauxM) / (1 - Math.Pow(1 + TauxM, dureeCalc)); //Calcul de la mensualite
+            ResuM = Math.Round(ResuM, 2);//Arrondi 10-2
+            PrixTotalCredit = ResuM * duree;//Calcul du prix de reviens du credit
+
+            ResultatMensu = ResuM.ToString();//Conversion Double to String
+            ResultatTotal = PrixTotalCredit.ToString();//Conversion Double to String
+
+            if (testSalaire(salaire, ResuM) == false)
+                MessageBox.Show("Vous ne gagnez pas assez d'argent par mois pour la durée de votre crédit");
+            else
+            {
+                dataGridView1.Visible = true;
+                for (int i = 2; i < duree + 1; i++)
+                {
+
+                    id = i.ToString(); //Numero du mois
+                    InteretMens = montant * TauxM; //interet 
+
+                    CapitalRembourse = ResuM - InteretMens;  //Capital Rembourse
+                    montant = (montant - ResuM) + InteretMens; //Nouveau Montant
+
+                    CapitalRembourse = Math.Round(CapitalRembourse, 2);//Arrondi 10-2
+                    InteretMens = Math.Round(InteretMens, 2);//Arrondi 10-2
+                    montant = Math.Round(montant, 2); //Arrondi 10-2
+
+                    string MontantRestant = montant.ToString(); //Conversion Double to String
+                    interetMensStr = InteretMens.ToString();//Conversion Double to String
+                    RemboursementStr = CapitalRembourse.ToString();//Conversion Double to String
+
+                    dataGridView1.Rows.Add(id, ResultatMensu, RemboursementStr, interetMensStr, MontantRestant); //Ajout dans le tableau
+                }
+            }
+        }
+
+        private Boolean testSalaire(double sal, double resu)
+        {
+            if (sal / 3 < resu)
+                return false;
+            return true;
+        }
+    }
+}
+
